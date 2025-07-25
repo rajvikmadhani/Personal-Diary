3656dfd0f5e1d7c14b88170712def75c4cb5a552
RajviMadhani <127208465+RajviMadhani@users.noreply.github.com>
2025-07-25 14:02:12 +0200

    fix:deploy site...

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Personal-Diary/", // ✅ This is crucial!
   plugins: [react()],
-  base: '/Personal-Diary/',  // 👈 this must match your repo name
-})
+});
